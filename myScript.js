b20bff8bf443b75e5b8782255cfb41e675488be8
vinceye <vinceye.cm@gmail.com>
2023-02-08 18:14:08 -0800

    added initial js to add book to DOM

--- myScript.js
+++ myScript.js
@@ -1,3 +1,6 @@
+"use strict";
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title
     this.author = author
@@ -14,5 +17,22 @@ function Book(title, author, pages, read) {
     }
 }
 
+function addBookToLibrary (book) {
+    myLibrary.push(book);
+}
+
 const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, true);
 console.log(theHobbit.info());
+
+console.log(myLibrary);
+addBookToLibrary(theHobbit);
+console.log(myLibrary);
+
+const bookDisplay = document.querySelector(".book-display");
+
+for (let i = 0; i < myLibrary.length; i++) {
+    let newBook = document.createElement('div');
+    newBook.className = 'book';
+    newBook.textContent = myLibrary[i].info(); 
+    bookDisplay.appendChild(newBook);
+}
